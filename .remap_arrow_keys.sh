bca81960015f4085a4c835d555b322f4e1a5235b
Krishna Kumar <krishna@Krishnas-MacBook-Pro.local>
2017-06-01 20:54:08 +0530

    Added hidutil command to remap arrow keys

---
+++ .remap_arrow_keys.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hidutil property --set '{"UserKeyMapping": [{"HIDKeyboardModifierMappingSrc":0x7000000e5, "HIDKeyboardModifierMappingDst":0x700000052},
+{"HIDKeyboardModifierMappingSrc":0x7000000e6, "HIDKeyboardModifierMappingDst":0x700000051}] }'
